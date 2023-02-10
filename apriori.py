ac3b1769895528435efdeb6f148f803d7e8678ec
MichOchieng <MichOchieng@gmail.com>
2023-02-09 17:12:59 -0800

    Updated create sets, added run times

--- apriori.py
+++ apriori.py
@@ -1,6 +1,6 @@
 import re
 import sys
-from math import ceil
+from math import floor
 from collections import Counter
 from itertools   import combinations
 from timeit import default_timer as timer
@@ -28,8 +28,7 @@ class Apriori:
         t0 = timer()
         self.FINAL_TABLE = self.apriori(self.TABLE,1)
         t1 = timer()
-        totRunTime = t1-t0
-        print(totRunTime)
+        self.getRunTime(t0,t1)
         
 
     def initTable(self):
@@ -37,7 +36,6 @@ class Apriori:
         for key in self.DATASET:
             counts = counts + Counter(self.DATASET[key])
         self.TABLE = dict(counts)
-        # print('Initial table: ',self.TABLE)
 
     def addSupports(self,table) -> dict:
         for dbKey in self.DATASET:
@@ -49,7 +47,10 @@ class Apriori:
 
     def createSets(self,table,k) -> list:
         # Create a set of all unique table values
-        setItems = set(value for key in table for value in key)
+        if k == 1:
+            setItems = set(key for key in table)
+        else:
+            setItems = set(value for key in table for value in key)
         # Return a list of combinations of size k given the above set items
         return list(combinations(setItems,k))
 
@@ -66,19 +67,24 @@ class Apriori:
         #   Create new Dictionary for combinations
         #   Add supports to dictionary        
         sets = self.createSets(table,k)
-        # print('Sets: ',sets)
         nextTable = dict.fromkeys(sets,0)
         nextTable = self.addSupports(nextTable)
-        # print("New Table: ",nextTable)
         # Prune table
         nextTable = self.prune(nextTable)
-        # print("Pruned next Table: ",nextTable,'\n')
+        # Check for final table
         if len(nextTable) > 0 and k < len(self.TABLE) + 1:
             k+=1
             self.apriori(nextTable,k)
         else:
             print('Final table: ',table)
             return table
+
+    def getRunTime(self,t0,t1):
+        totalRunTime = t1-t0
+        if totalRunTime < 1000:
+            print('Total runtime: ',round(totalRunTime*1000,3),'ms')
+        else:
+            print('Total runtime: ',round(totalRunTime,3),'sec')
 
     def readFile(self,file:str): 
         try:
@@ -91,14 +97,14 @@ class Apriori:
                 # Init num transactions
                 self.NUM_TRANSACTIONS = int(lines[0])
                 # Init support
-                self.SUPPORT = ceil(self.NUM_TRANSACTIONS * (self.SUPPORT_LEVEL / 100))
+                self.SUPPORT = floor(self.NUM_TRANSACTIONS * (self.SUPPORT_LEVEL / 100))
                 print("Support level: ",self.SUPPORT)
                 for line in lines[1:]:
                     row = re.split(r'\t+',line)
                     items = row[-1].rstrip('\r\n')
                     itemList = items.split()
                     self.DATASET[row[0]] = itemList
-            # print('Initial dataset: ',self.DATASET)
+            print('Initial dataset: ',self.DATASET)
         except FileNotFoundError:
             print("Couldn't find file:",file,':(')
             exit()
